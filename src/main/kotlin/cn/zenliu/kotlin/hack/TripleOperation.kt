63bc92a8f1ef9f27d7d73e9708649954099e2494
Zen Liu <lcz20@163.com>
2018-12-21 10:04:17 +0800

    Update TripleOperation.kt

--- src/main/kotlin/cn/zenliu/kotlin/hack/TripleOperation.kt
+++ src/main/kotlin/cn/zenliu/kotlin/hack/TripleOperation.kt
@@ -1,36 +1,33 @@
 @file:Suppress("NOTHING_TO_INLINE")
 
 package cn.zenliu.kotlin.hack
-
 /**
  *  (a to b)% boolean
  * @receiver Pair<Any?,Any?>
  * @param b Boolean
  * @return Any?
  */
-inline operator fun Pair<Any?, Any?>.rem(b: Boolean) = if (b) this.first else this.second
+inline operator fun Pair<Any?, Any?>.rem(b: Boolean) = if (b) when(this.first){
+    is Function0<*>->(this.first as Function0<*>).invoke()
+    else->this.first
+} else when(this.second){
+    is Function0<*>->(this.second as Function0<*>).invoke()
+    else->this.second
+}
+
+
 
 /**
- *  (boolean)%(a to b) like AnyBoolean?a:b
+ * (boolean)%(a to b)
  * @receiver Boolean
- * @param value Pair<Any?, Any?>
+ * @param value Pair<Any?,Any?>
  * @return Any?
  */
-inline operator fun Boolean.rem(value: Pair<Any?, Any?>) = if (this) value.first else value.second
+inline operator fun Boolean.rem(value: Pair<Any?,Any?>) = if (this) when(value.first){
+    is Function0<*>->(value.first as Function0<*>).invoke()
+    else->value.first
+} else when(value.second){
+    is Function0<*>->(value.second as Function0<*>).invoke()
+    else->value.second
+}
 
-/**
- * (boolean)%({} to {})
- * @receiver Boolean
- * @param value Pair<()->Unit, ()->Unit>
- */
-inline operator fun Boolean.rem(value: Pair<()->Unit, ()->Unit>) = if (this) value.first.invoke() else value.second.invoke()
-
-/**
- *
- * @receiver Pair<()->Unit, ()->Unit>
- * @param b Boolean
- * @return Function0<Unit>
- */
-inline operator fun Pair<()->Unit, ()->Unit>.rem(b: Boolean) = if (b) this.first else this.second
-
-//inline operator fun <T,R>Pair<()->T, ()->R>.rem(b: Boolean) = if (b) this.first else this.second
